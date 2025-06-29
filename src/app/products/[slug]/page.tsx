3068ed2a1624bddc2220c3e6b73be196c3ac663e
hocasiones <hocasiones@gmail.com>
2025-06-29 14:52:40 +0800

    Address Single Product Page Reload

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,11 +1,42 @@
 "use client"
 
+import directus from "@/directus/directus"
+import Fragments from "@/graphql/fragments"
+import useStore from "@/store/store"
 import { Grid } from "@mantine/core"
+import { useQuery } from "@tanstack/react-query"
+import { use, useEffect } from "react"
 import ProductCarousel from "./Components/ProductCarousel"
 import ProductDetails from "./Components/ProductDetails"
 import ProductTabs from "./Components/ProductTabs"
 
-const Page = () => {
+const Page = ({ params }: { params: Promise<{ slug: string }> }) => {
+	const { slug } = use(params)
+	const product = useStore((state: any) => state.singleProduct)
+	const setSingleProduct = useStore((state: any) => state.setSingleProduct)
+
+	const query = useQuery({
+		queryKey: ["product", product?.id],
+		queryFn: async () => {
+			const { products_by_id } = await directus.query(`
+					${Fragments}
+					query {
+						products_by_id (id: "${slug}") {
+							...ProductFragment
+						}
+					}
+				`)
+			return products_by_id
+		},
+		enabled: !product,
+	})
+
+	useEffect(() => {
+		if (query.isFetched && query.data) {
+			setSingleProduct({ ...query.data })
+		}
+	}, [query.isFetched, query.data, setSingleProduct])
+
 	return (
 		<Grid gutter="xl">
 			<Grid.Col span={{ base: 12, md: 6 }}>
--- src/directus/directus.ts
+++ src/directus/directus.ts
@@ -2,7 +2,7 @@ import { Schema } from "@/Types/types"
 import { createDirectus, graphql } from "@directus/sdk"
 
 const directus = createDirectus<Schema>(
-	`${process.env.NEXT_PUBLIC_API_URL}`
+	`${process.env.NEXT_PUBLIC_SERVER_URL}`
 ).with(graphql())
 
 export default directus
